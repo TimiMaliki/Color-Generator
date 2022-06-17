76b5ffda139b1b41017941ded448ea0feffa3074
TimiMaliki <malikitimi@gmail.com>
2022-06-17 19:55:55 +0100

    pratice

--- src/js/remake.js
+++ src/js/remake.js
@@ -1,11 +1,10 @@
 const button = document.querySelector('.tx');
-const bgC= document.querySelector('body');
+const bgC = document.querySelector('body');
 const pTag = document.querySelector('p');
 
-
 button.addEventListener('click',() =>{
- let theColor = '';
-  theColor+= Math.random().toString(16).slice(2,8);
+ let theColor ='#';
+ theColor+= Math.random().toString(16).slice(2,8);
  bgC.style.backgroundColor = theColor;
  pTag.innerText = theColor;
-})
+});
