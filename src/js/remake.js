ed2c62e3e21b5525311a77b6d9d51b3bc2a6925f
TimiMaliki <malikitimi@gmail.com>
2022-06-17 20:18:40 +0100

    pratice

--- src/js/remake.js
+++ src/js/remake.js
@@ -1,10 +1,20 @@
-const text = document.querySelector('.tx');
+const button = document.querySelector('.tx');
 const bgC = document.querySelector('body');
 const pTag = document.querySelector('p');
 
-text.addEventListener('click',() =>{
+button.addEventListener('click',() =>{
  let theColor ='#';
  theColor+= Math.random().toString(16).slice(2,8);
  bgC.style.backgroundColor = theColor;
  pTag.innerText = theColor;
 });
+ // Image using javascript
+ function btn(){
+       
+
+    let image = document.getElementById('image');
+    let images = ['pics/img1.jpg','pics/img2.jpg','pics/img3.jpg','pics/img4.jpg','pics/img5.jpg',];
+
+    let random = Math.floor(Math.random() * 5);
+    image.src = images[random];
+}
